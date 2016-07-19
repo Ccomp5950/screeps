e0a50b02f176c7b29381e7a254f5276a2e7e5132
Ccomp5950 <ccomp5950@gmail.com>
2016-07-19 01:54:44 -0400

    jfakjdfkjdf

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ module.exports.loop = function () {
         // get the creep object
         var creep = Game.creeps[name];
 
-	if(underAttack == true || creep.memory.role = 'towertender') {
+	if(underAttack || creep.memory.role == 'towertender') {
 		roleTowerTender.run(creep);
 	}
 	
