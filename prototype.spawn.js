08de13e5b2f35edc6869ae4d0f4c6db60e15b250
Ccomp5950 <ccomp5950@gmail.com>
2016-07-19 00:28:52 -0400

    changes spawn parts

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -5,15 +5,17 @@ module.exports = function() {
             // create a balanced body as big as possible with the given energy
             var numberOfParts = Math.floor(energy / 200);
             var body = [];
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(WORK);
-            }
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(CARRY);
-            }
-            for (let i = 0; i < numberOfParts; i++) {
-                body.push(MOVE);
-            }
+	    switch(numberOfParts) {
+            case 1:
+	    	body = [WORK, CARRY, CARRY, MOVE];
+		break;
+	    case 2:
+		body = [WORK,WORK,CARRY,CARRY,CARRY,MOVE];
+		break;
+	    case 3:
+		body = [WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE];
+		break;
+	    }
 
             // create creep with the created body and the given role
             return this.createCreep(body, undefined, { role: roleName, working: false, source: null });
