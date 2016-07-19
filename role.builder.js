0034da3540f59f0c8d20f25855b2f684826bb564
Ccomp5950 <ccomp5950@gmail.com>
2016-07-18 20:56:02 -0400

    Optimized builder II

--- role.builder.js
+++ role.builder.js
@@ -20,7 +20,7 @@ module.exports = {
         if (creep.memory.working == true) {
             // find closest constructionSite
             var constructionSite = null;
-	    if(!creep.memory.build && !getObjectById(creep.memory.build)) {
+	    if(!creep.memory.build && !Game.getObjectById(creep.memory.build)) {
 		var constructionSite = creep.pos.findClosestByPath(FIND_CONSTRUCTION_SITES);
 		creep.memory.build = constructionSite.id
 	    }
