8926042af608dcf72853065247327cc988c544fe
Ccomp5950 <ccomp5950@gmail.com>
2016-07-19 00:55:02 -0400

    Changes up repairers to be special snowflakes that move fast

--- prototype.spawn.js
+++ prototype.spawn.js
@@ -5,6 +5,25 @@ module.exports = function() {
             // create a balanced body as big as possible with the given energy
             var numberOfParts = Math.floor(energy / 200);
             var body = [];
+	    var bodyset = false;
+	if(roleName == "repairer") {
+		switch(numberOfParts) {
+		
+		case 1:
+			body = [WORK,CARRY,MOVE,MOVE];
+			break;
+		case 2:
+			body = [WORK,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE];
+			break;
+		case 3:
+			body = [WORK,WORK,CARRY,CARRY,CARRY,MOVE,MOVE,MOVE,MOVE,MOVE];
+			break;		
+
+		}
+		bodyset = true;
+	}
+	
+	if(!bodyset) {
 	    switch(numberOfParts) {
             case 1:
 	    	body = [WORK, CARRY, CARRY, MOVE];
@@ -16,6 +35,7 @@ module.exports = function() {
 		body = [WORK,WORK,WORK,CARRY,CARRY,CARRY,CARRY,MOVE,MOVE];
 		break;
 	    }
+	}
 
             // create creep with the created body and the given role
             return this.createCreep(body, undefined, { role: roleName, working: false, source: null });
