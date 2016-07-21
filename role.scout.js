0ed4b45ec7eb0dd22d6be30f916f8c2a9b81e89a
Ccomp5950 <ccomp5950@gmail.com>
2016-07-21 00:26:42 -0400

    well fuck heres debugging

--- role.scout.js
+++ role.scout.js
@@ -3,7 +3,9 @@ module.exports = {
     run: function(creep) {
 		if(!Game.flags.scout1 === undefined) {
 			var range = creep.pos.getRangeTo(Game.flags.scout1);
+			console.log("Found the flag at range: " + range);
 			if(range > 2) {
+				console.log "And that is further then 2");
 				creep.moveTo(Game.flags.scout1);
 			}
 		}
