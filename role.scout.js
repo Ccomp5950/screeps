a25a29740c7e139564e4e5d8360053c42cb0a995
Ccomp5950 <ccomp5950@gmail.com>
2016-07-21 00:21:19 -0400

    jkdfakljfda

--- role.scout.js
+++ role.scout.js
@@ -2,7 +2,8 @@ module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
 		if(!Game.flags.scout1 === undefined) {
-			if(creep.getRangeTo(Game.flags.scout1) > 2) {
+			var range = creep.pos.getRangeTo(Game.flags.scout1);
+			if(range == Infinity || range > 2) {
 				creep.moveTo(Game.flags.scout1);
 			}
 		}
