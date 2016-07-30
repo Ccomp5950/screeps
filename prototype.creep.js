87ea0447c48278c5172469a0c4f8b97ab243add6
Ccomp5950 <ccomp5950@gmail.com>
2016-07-30 03:30:56 -0400

    jfdakjdfa

--- prototype.creep.js
+++ prototype.creep.js
@@ -33,7 +33,8 @@ module.exports = function() {
                             }
 
 		    }
-		    else if(source.structureType == STRUCTURE_SOURCE) {
+			//TODO: FIX THIS SHIT
+		    else if(source.structureType == != undefined) {
 		            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
 		                if(creep.moveTo(source) == ERR_NO_PATH) {
 					creep.memory.source = null;
