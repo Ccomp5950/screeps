8d34ca2c90a327e0e7ca35bc4945235348db6e84
Ccomp5950 <ccomp5950@gmail.com>
2016-07-30 03:49:26 -0400

    jfdasfa

--- role.harvester.js
+++ role.harvester.js
@@ -32,7 +32,7 @@ module.exports = {
             });
             if (structure == undefined) {
              	structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                        filter: (s) => (s.structureType == STRUCTURE_STORAGE)
+                        filter: (s) => (s.structureType == STRUCTURE_STORAGE || s.structureType == STRUCTURE_CONTAINER)
                              && s.store[RESOURCE_ENERGY] < s.storeCapacity
             });
 	    }
