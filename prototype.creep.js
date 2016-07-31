a811ccffe4ef8f519f53145b1d1ea3977592a4c4
Ccomp5950 <ccomp5950@gmail.com>
2016-07-31 12:25:42 -0400

    fdafas

--- prototype.creep.js
+++ prototype.creep.js
@@ -96,7 +96,7 @@ module.exports = function() {
 		if(creep.pos.getRangeTo(Game.spawns.Spawn1) > 1) {
 			creep.moveTo(Game.spawns.Spawn1);
 		}
-		if(Game.spawns.Spawn1.spawning = null) {
+		if(Game.spawns.Spawn1.spawning == null) {
 			let i = Game.spawns.Spawn1.renewCreep(creep);
 			console.log("renew gave error: " + i);
 			creep.memory.restoring = false;
