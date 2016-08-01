8a9cb8d80d9431b643922d1e506a261b1ffe9886
Ccomp5950 <ccomp5950@gmail.com>
2016-07-31 23:54:39 -0400

    fdafsa

--- role.fetcher.js
+++ role.fetcher.js
@@ -4,10 +4,13 @@ module.exports = {
                 if(creep.spawning) {
                         return;
                 }
+		
 		let container = Game.getObjectById(creep.memory.container);
 		let storage = creep.room.storage;
-		if(container.store[RESOURCE_ENERGY] < 50) {
+		if(container != null && container.store[RESOURCE_ENERGY] < 50) {
 			creep.memory.switchOnce = true;
+		} else {
+			
 		}
 		
 	        if(creep.ticksToLive < 100) {
@@ -37,13 +40,18 @@ module.exports = {
 					
 			if(creep.memory.container == null) {
 				creep.memory.container = null;
+			} else {
 			}
+
 			if(creep.memory.container == null || container == null || creep.memory.switchOnce == true) {
 		          	container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
 	                        filter: (s) => (s.structureType == STRUCTURE_CONTAINER)
 					});
 			}
 			if(container != null) {
+				if(creep.memory.container == null) {
+					creep.memory.container = container.id
+				}
 				creep.withdraw(container, RESOURCE_ENERGY);
 			} else {
 				console.log("Fuck " + creep.name + "can't find his container");
