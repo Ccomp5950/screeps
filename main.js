7a5b1d11568cfc2eca4fc01f4719f39798a07122
Ccomp5950 <ccomp5950@gmail.com>
2016-08-01 10:33:02 -0400

    fdaf

--- main.js
+++ main.js
@@ -205,7 +205,7 @@ module.exports.loop = function () {
     var minimumNumberOfScouts = 0;
     var minimumNumberOfAttackers = 0;
     var minimumNumberOfDefenders = 1;
-    var minimumNumberOfRemoteHarvesters = 22;
+    var minimumNumberOfRemoteHarvesters = 6;
 
     // count the number of creeps alive for each role
     // _.sum will count the number of properties in Game.creeps filtered by the
