d670efc57913f34fd0c36bb678a021d114ea07c9
Ccomp5950 <ccomp5950@gmail.com>
2016-08-02 01:40:19 -0400

    adsfasfdas

--- main.js
+++ main.js
@@ -110,7 +110,7 @@ module.exports.loop = function () {
         }
         else {
 		if(roles[creep.memory.role] == null) {
-			console.log("Warning: " + name + " has a bad role: " creep.memory.role);
+			console.log("Warning: " + name + " has a bad role: " + creep.memory.role);
 		} else {
 			roles[creep.memory.role].current++;
 			roles[creep.memory.role].run.run(creep);
