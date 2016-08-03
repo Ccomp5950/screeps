ca3ad95f0374bc201f688948d9f38edf9d7a011f
Ccomp5950 <ccomp5950@gmail.com>
2016-08-03 02:16:28 -0400

    afafafa

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ module.exports.loop = function () {
 
 var roles =            {harvester:      {namer:"harvester",              minimum:2,      requirement:0,          buildRestriction : false,	run: require('role.harvester')},
                         miner:          {namer:"miner",                  minimum:3,      requirement:900,        buildRestriction : true,	run: require('role.miner')},
-                        fetcher:        {namer:"fetcher",                minimum:2,      requirement:850,        buildRestriction : true,	run: require('role.fetcher')},
+                        fetcher:        {namer:"fetcher",                minimum:3,      requirement:850,        buildRestriction : true,	run: require('role.fetcher')},
                         upgrader:       {namer:"upgrader",               minimum:2,      requirement:-1,         buildRestriction : true,	run: require('role.upgrader')},
                         builder:        {namer:"builder",                minimum:1,      requirement:0,          buildRestriction : true,	run: require('role.builder')},
                         repairer:       {namer:"repairer",               minimum:1,      requirement:0,          buildRestriction : true,	run: require('role.repairer')},
