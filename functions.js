b61f8c4f9a5ce839399cd8d575051b389f4ec32b
Ccomp5950 <ccomp5950@gmail.com>
2016-08-03 04:36:18 -0400

    fdafa

--- functions.js
+++ functions.js
@@ -9,4 +9,20 @@
 		
 		}
 		return count;
+	};
+
+	global.displayRoles =
+	function() {
+		var rolecount = new Object();
+		for(roleM in roles) {
+			rolecount[roles[roleM].namer] = 0;
+		}
+		for(creepM in Game.creeps) {
+			rolecount[Game.creeps[creepM].role]++;
+		}
+		for(roleM in roles) {
+			console.log("You have " + rolecount[roles[roleM].namer] + " " + roles[roleM].namer +"'s creeps");
+		}
 	}
+
+	
