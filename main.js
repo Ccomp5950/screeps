c8420422521056d3738d867b55a9129fe978aaef
Ccomp5950 <ccomp5950@gmail.com>
2016-08-04 19:21:35 -0400

    fdsafads

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ require('prototype.flag')();
 require('functions');
 var roles =            {harvester:      {namer:"harvester",             minimum:2,      requirement:0,          buildRestriction : false,       run: require('role.harvester')},
 			sapper:         {namer:"sapper",                minimum:1,      requirement:-1,         buildRestriction : true,        run: require('role.sapper')},
-                        towerdrainer:   {namer:"towerdrainer",          minimum:0,      requirement:2300,       buildRestriction : true,        run: require('role.towerdrainer')},
+                        towerdrainer:   {namer:"towerdrainer",          minimum:2,      requirement:2300,       buildRestriction : true,        run: require('role.towerdrainer')},
                         healer:         {namer:"healer",                minimum:0,      requirement:1500,       buildRestriction : true,        run: require('role.healer')},
                         miner:          {namer:"miner",                 minimum:4,      requirement:900,        buildRestriction : true,        run: require('role.miner')},
                         fetcher:        {namer:"fetcher",               minimum:4,      requirement:1400,       buildRestriction : true,        run: require('role.fetcher')},
