9788f483183769d0503da7740a36b7a1769d09cd
Ccomp5950 <ccomp5950@gmail.com>
2016-08-04 15:37:53 -0400

    fdfadfasfas

--- main.js
+++ main.js
@@ -158,7 +158,7 @@ module.exports.loop = function () {
 		if(roles[creep.memory.role] == null) {
 			console.log("Warning: " + name + " has a bad role: " + creep.memory.role);
 		} else {
-			if(creeps.ticksToLive == undefined && (creep.ticksToLive - (creep.body.length * 2) - 2) >= 0) {
+			if(creep.ticksToLive == undefined && (creep.ticksToLive - (creep.body.length * 2) - 2) >= 0) {
 				roles[creep.memory.role].current++;
 			}
 			roles[creep.memory.role].run.run(creep);
