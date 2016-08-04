11705dbb28e421b3dbed16613de27609361c4b87
Ccomp5950 <ccomp5950@gmail.com>
2016-08-04 08:15:26 -0400

    fdasfa

--- main.js
+++ main.js
@@ -12,7 +12,7 @@ var roles =            {harvester:      {namer:"harvester",             minimum:
                         repairer:       {namer:"repairer",              minimum:1,      requirement:0,          buildRestriction : true,        run: require('role.repairer')},
                         wallrepairer:   {namer:"wallrepairer",          minimum:1,      requirement:0,          buildRestriction : true,        run: require('role.wallRepairer')},
                         towertender:    {namer:"towertender",           minimum:0,      requirement:0,          buildRestriction : false,       run: require('role.towertender')},
-                        scout:          {namer:"scout",                 minimum:1,      requirement:200,        buildRestriction : true,        run: require('role.scout')},
+                        scout:          {namer:"scout",                 minimum:0,      requirement:200,        buildRestriction : true,        run: require('role.scout')},
                         attacker:       {namer:"attacker",              minimum:0,      requirement:800,        buildRestriction : false,       run: require('role.attacker')},
                         defender:       {namer:"defender",              minimum:1,      requirement:-1,		buildRestriction : false,       run: require('role.defender')},
                         raider:         {namer:"raider",                minimum:0,      requirement:800,        buildRestriction : false,       run: require('role.raider')},
