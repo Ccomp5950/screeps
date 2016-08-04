1e24c683740732e25a06b32f7ebd2221d0c7f838
Ccomp5950 <ccomp5950@gmail.com>
2016-08-04 18:03:35 -0400

    only one upgrader

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ require('functions');
 var roles =            {harvester:      {namer:"harvester",             minimum:2,      requirement:0,          buildRestriction : false,       run: require('role.harvester')},
                         miner:          {namer:"miner",                 minimum:4,      requirement:900,        buildRestriction : true,        run: require('role.miner')},
                         fetcher:        {namer:"fetcher",               minimum:4,      requirement:1400,       buildRestriction : true,        run: require('role.fetcher')},
-                        upgrader:       {namer:"upgrader",              minimum:2,      requirement:-1,         buildRestriction : true,        run: require('role.upgrader')},
+                        upgrader:       {namer:"upgrader",              minimum:1,      requirement:-1,         buildRestriction : true,        run: require('role.upgrader')},
                         builder:        {namer:"builder",               minimum:0,      requirement:0,          buildRestriction : true,        run: require('role.builder')},
                         repairer:       {namer:"repairer",              minimum:1,      requirement:0,          buildRestriction : true,        run: require('role.repairer')},
                         wallrepairer:   {namer:"wallrepairer",          minimum:1,      requirement:0,          buildRestriction : true,        run: require('role.wallRepairer')},
