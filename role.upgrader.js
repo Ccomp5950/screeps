859c3080ffb3f6df05829155e4320f24a8d3ca72
Ccomp5950 <ccomp5950@gmail.com>
2016-08-05 21:27:10 -0400

    fdafdas

--- role.upgrader.js
+++ role.upgrader.js
@@ -31,6 +31,7 @@ module.exports = {
         }
         // if creep is supposed to harvest energy from source
         else {
+		creep.approachAssignedFlag(0);
 		if(creep.memory.container == null || container == null || creep.memory.switchOnce == true) {
 				container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
 					filter: (s) => (s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] > 10)
