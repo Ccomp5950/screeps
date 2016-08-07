16b9ca065684374ce7f5b8784020f1eaf71c20fe
Ccomp5950 <ccomp5950@gmail.com>
2016-08-07 02:00:55 -0400

    removes controllers from attack choices for attacker

--- role.attacker.js
+++ role.attacker.js
@@ -63,7 +63,7 @@ module.exports = {
                                 }
                 return;
                 }
-                target = creep.pos.findClosestByPath(FIND_STRUCTURES);
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES, { filter: (s) => s.structureType != STRUCTURE_CONTROLLER});
                 if (target != undefined) {
                             if (creep.attack(target) == ERR_NOT_IN_RANGE) {
                                 creep.moveTo(target, {maxRooms:1});
