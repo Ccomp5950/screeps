129c9451973aa784ec3e92d917c92020acc83eb4
Ccomp5950 <ccomp5950@gmail.com>
2016-08-07 08:02:19 -0400

    fdfdafa

--- role.harvester.js
+++ role.harvester.js
@@ -14,33 +14,25 @@ module.exports = {
             // switch state
             creep.memory.working = false;
         }
-        // if creep is harvesting energy but is full
         else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
-            // switch state
             creep.memory.working = true;
             creep.memory.source = null;
         }
 
-        // if creep is supposed to transfer energy to a structure
         if (creep.memory.working == true) {
 		if(creep.carry.energy < creep.carryCapacity && creep.pos.getRangeTo(creep.room.storage) < 2) {
 			creep.withdraw(creep.room.storage, RESOURCE_ENERGY)
 		}
-            // find closest spawn, extension or tower which is not full
             var structure = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-                // the second argument for findClosestByPath is an object which takes
-                // a property called filter which can be a function
-                // we use the arrow operator to define it
                 filter: (s) => (s.structureType == STRUCTURE_EXTENSION
  			     || s.structureType == STRUCTURE_SPAWN
                              || s.structureType == STRUCTURE_TOWER)
-				
                              && s.energy < s.energyCapacity
-			     // && s.isBeingHandled(creep) == false
+			     && s.isBeingHandled(creep) == false
             });
 	    if (structure == null) {
 		structure = Game.flags["upgraderContainer"].pos.findClosestByRange(FIND_STRUCTURES, {
-			filter:(s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] < 1000 // && s.isBeingHandled(creep) == false
+			filter:(s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] < 1000 && s.isBeingHandled(creep) == false
 			
 		 }); 
 		if(structure.pos.getRangeTo(Game.flags["upgraderContainer"].pos) > 2) {
@@ -53,8 +45,6 @@ module.exports = {
              	structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                         filter: (s) => (s.structureType == STRUCTURE_STORAGE
                              && s.store[RESOURCE_ENERGY] < s.storeCapacity && s.id != creep.memory.pulledfrom)
-		//		|| (s.structureType == STRUCTURE_SPAWN
-		//	     && s.energy < s.energyCapacity)
 
             });
 	    }
