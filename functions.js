19d87c7feb038f0eb5e989336bd7602f9c802d63
Ccomp5950 <ccomp5950@gmail.com>
2016-08-13 05:52:55 -0400

    fdfdfa

--- functions.js
+++ functions.js
@@ -57,7 +57,7 @@
 			if(flag.room.controller.reservation != undefined) {
 				ticks = flag.room.controller.reservation.ticksToEnd;
 			}
-			console.log("Checking: " + flag.name + " and  it has " + ticks + " before reservation drops");
+			console.log("Checking: " + flag.name + " and  it has " + ticks + " before reservation drops (flag active is: "+ flag.active);
 			if(flag.active == true && ticks < 4000) {
 				needed++;
 			}
