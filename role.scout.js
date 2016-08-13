d103e3d3cd47d73917988b3297e0bf1268f2d18b
Ccomp5950 <ccomp5950@gmail.com>
2016-08-13 03:53:15 -0400

    fdasfas

--- role.scout.js
+++ role.scout.js
@@ -2,8 +2,19 @@ module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
                 if(creep.spawning) {
+			creep.memory.waypoint = 1;
                         return;
                 }
+		if(creep.memory.waypoint != -1 && Game.flags[creep.memory.waypoint] != undefined) {
+			var flag = Game.flags[creep.memory.waypoint];
+			var range = creep.pos.getRangeTo(flag);
+			if(range > 0) {
+				creep.moveTo(flag);
+				return;
+			} else {
+				creep.memory.waypoint += 1;
+			}
+		}
                 if(creep.getAwayFromEdge()) {
                         return;
                 }
