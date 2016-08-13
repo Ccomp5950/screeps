423a8d222319d2d03cb70ec62576b60027cf101e
Ccomp5950 <ccomp5950@gmail.com>
2016-08-13 05:59:17 -0400

    fdasfda

--- prototype.flag.js
+++ prototype.flag.js
@@ -42,6 +42,31 @@ module.exports = function() {
                 }
         return true;
         };
+	Flag.prototype.needsClaimer =
+	function() {
+			let flag = this;
+                        var ticks = 0;
+                        if(flag.room.controller.reservation != undefined) {
+                                ticks = flag.room.controller.reservation.ticksToEnd;
+                        }
+                        if(flag.memory.active == true && ticks < 4000 && flag.hasClaimer() == false) {
+                                return true;
+                        }
+	return false;
+
+	]
+
+	Flag.prototype.setRoom =
+	function(roomString, force) {
+		let flag = this;
+		if(flag.memory.spawn == undefined) {
+			if(Game.rooms[roomString] == undefined) {
+				console.log("Error, assigning " + roomString + " to " + flag.name + "Room undefined");
+			}
+
+			flag.memory.spawn = roomString
+		}
+	}
 
 
 
