483840e1cb7c25216f065cd6dbba6a2782380385
Ccomp5950 <ccomp5950@gmail.com>
2016-08-14 02:21:23 -0400

    fdasfda

--- role.attacker.js
+++ role.attacker.js
@@ -22,6 +22,7 @@ module.exports = {
 		}
 		if(creep.memory.ready == true) {
 		*/
+		if(creep.gotoWaypoint()) return;
                 if(creep.getAwayFromEdge()) {
                         return;
                 }
