7075d76ebff131347938d8f10779770909ffb59f
Ccomp5950 <ccomp5950@gmail.com>
2016-08-14 22:17:31 -0400

    fdasfdasfa

--- functions.js
+++ functions.js
@@ -74,7 +74,7 @@
 					room.links = {};				
 		                        for(let targetM in targets) {
 						let target = targets[targetM];
-						let linkPos = target.pos.x.toString + "_" + target.pos.y.toString;	
+						let linkPos = target.pos.x.toString() + "_" + target.pos.y.toString();	
 						room.links[linkPos] = {};
 						room.links[linkPos].id = target.id;
 						room.links[linkPos].priority = 0;
