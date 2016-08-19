f867c51eb6fc6e184a93c0cd56884c2df6df23dd
Ccomp5950 <ccomp5950@gmail.com>
2016-08-18 21:47:58 -0400

    fdasfdfa

--- role.upgradertinder.js
+++ role.upgradertinder.js
@@ -23,23 +23,20 @@ module.exports = {
 		if(creep.memory.working == false) {
 				var storage = creep.room.storage;
 				if(storage == undefined) {
-					creep.say(":( :( :(");
-					console.log("[" + creep.name + "] can't find storage");
+					creep.customharvest(creep);
 					return;
 				}
 				if(creep.withdraw(storage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
 					creep.moveTo(storage);
 				};
 		}else {
-			var flag = Game.flags["upgraderContainer"];
+			var flagname = "upgraderContainer";
+                        var flags = creep.room.find(FIND_FLAGS, function(o) { if(o.name.substr(0,flagname.length) == flagname) return true})
+                        var flag = flags[0];
 			if(creep.pos.getRangeTo(flag) > 1) {
-				creep.moveTo(Game.flags["upgraderContainer"]);
+				creep.moveTo(flag);
 				return;
 			}
-	                flagname = "upgraderContainer";
-	                flags = creep.room.find(FIND_FLAGS, function(o) { if(o.name.substr(0,flagname.length) == flagname) return true})
-	                flag = flags[0];
-
 			var target = _(flag.pos.findInRange(FIND_STRUCTURES, 2)).filter((s) => s.structureType == STRUCTURE_CONTAINER && s.store[RESOURCE_ENERGY] < 1200).sortBy(s=> _.sum(s.store)).first();
 			if(target != undefined) {
 				if(creep.pos.getRangeTo(target) > 1) {
