2f3140e6e3f6bc97115dab7935edcb2b76dad313
Ccomp5950 <ccomp5950@gmail.com>
2016-08-20 02:17:30 -0400

    fdafdaa

--- memorymgmt.js
+++ memorymgmt.js
@@ -42,7 +42,7 @@ module.exports = {
 
 	newFlags: function() {
 		for (let name in Game.flags) {
-			if(Game.flags[name].memory == null) {
+			if(Game.flags[name].memory == undefined) {
 				Game.flags[name].memory = {};
 				Game.flags[name].memory.active = false;
 				Game.flags[name].memory.spawn = Game.flags[name].pos.room;
