fa9463b1eb26e19bf5f828253cad687bda56b7f6
Ccomp5950 <ccomp5950@gmail.com>
2016-08-20 02:26:56 -0400

    fdasfda

--- memorymgmt.js
+++ memorymgmt.js
@@ -48,6 +48,7 @@ module.exports = {
 		        }
 			if(Game.flags[name].memory.active == undefined) {
 				Game.flags[name].memory.active = false;
+				continue;
 			}
 			if(Game.flags[name].memory.spawn == undefined) {
 				Game.flags[name].memory.spawn = Game.flags[name].pos.room;
