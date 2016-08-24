fc5252c3a4d90c25639cdfca4c400d325cfa24e3
Ccomp5950 <ccomp5950@gmail.com>
2016-08-24 01:48:11 -0400

    fdaffa

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ module.exports.loop = function () {
 	if(Game.time % 10 == 0) { 
 		memorymgmt.newRoles(roles);
 		if(Game.cpu.bucket < 9950) {
-			console.log("[" + Game.tick + "] <p style='color: red;'>" +Game.cpu.bucket + "</p>");
+			console.log("[" + Game.time + "] <span style='color: red;'>" +Game.cpu.bucket + "</span>");
 		}
 	}
 
