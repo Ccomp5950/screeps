9232879b561de20b1aae29b04c541200b1927145
Ccomp5950 <ccomp5950@gmail.com>
2016-08-28 16:33:44 -0400

    fdasfdsfa

--- functions.js
+++ functions.js
@@ -102,4 +102,19 @@
 		test123 = ["lala3", "lala4"];
 	return true;
 	}
+	global.timeStamp =
+	function() {
+	  var now = new Date();
+	  var date = [ now.getMonth() + 1, now.getDate(), now.getFullYear() ];
+	  var time = [ now.getHours(), now.getMinutes(), now.getSeconds() ];
+	  var suffix = ( time[0] < 12 ) ? "AM" : "PM";
+	  time[0] = ( time[0] < 12 ) ? time[0] : time[0] - 12;
+	  time[0] = time[0] || 12;
+	  for ( var i = 1; i < 3; i++ ) {
+	    if ( time[i] < 10 ) {
+	      time[i] = "0" + time[i];
+	    }
+	  }
+	  return date.join("/") + " " + time.join(":") + " " + suffix;
+	}
 	
