357ad719de6ca4b421553c97bb1595fe11667118
Ccomp5950 <ccomp5950@gmail.com>
2016-08-29 22:32:40 -0400

    fadfdafa

--- role.linktender.js
+++ role.linktender.js
@@ -91,9 +91,9 @@ module.exports = {
 				console.log("[" + creep.name + "] Unable to process energy full up.");
 				return;
 			} else { // Carrying Resources
-				creep.say("here");
 				if(hasTerminal && _.sum(terminal.store) < terminal.storeCapacity) {
-			                for(let resourceType in creep.carry) {
+			                creep.say("here");
+					for(let resourceType in creep.carry) {
 			                        creep.transfer(terminal, resourceType);
 						return;
 			                }
