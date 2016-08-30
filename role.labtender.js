51d40706383e86d715154ee625ecf473dbb8e3cb
Ccomp5950 <ccomp5950@gmail.com>
2016-08-30 03:08:46 -0400

    fdasfdsfa

--- role.labtender.js
+++ role.labtender.js
@@ -91,7 +91,7 @@ module.exports = {
 					continue;
 				}
 				if(lab.mineralAmount < 2000 && labMem.react == false) {
-					if(hasStorage && storage.store[labMem.mineral] != undefined && storage.store[labMem.mineral] > 1000) {
+					if(hasStorage && storage.store[labMem.mineral] != undefined && storage.store[labMem.mineral] > 400) {
 						creep.memory.lab = lab.id;
 						creep.memory.loading = labMem.mineral;
 						creep.memory.loading_from = "storage";
@@ -99,7 +99,7 @@ module.exports = {
 							creep.moveTo(storage);
 						}
 						return;
-					} else if(hasTerminal && terminal.store[labMem.mineral] != undefined && terminal.store[labMem.mineral] > 1000) {
+					} else if(hasTerminal && terminal.store[labMem.mineral] != undefined && terminal.store[labMem.mineral] > 400) {
 						creep.memory.lab = lab.id;
 						creep.memory.loading = labMem.mineral;
 						creep.memory.loading_from = "terminal";
