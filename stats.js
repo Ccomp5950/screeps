d70541ad86dfc7e3317de3d3da819a585434c365
Ccomp5950 <ccomp5950@gmail.com>
2016-08-31 21:39:55 -0400

    fdasfdfda

--- stats.js
+++ stats.js
@@ -50,6 +50,6 @@ Memory.stats['cpu.Start'] = startOfMain
 */
 Memory.stats['cpu.bucket'] = Game.cpu.bucket
 Memory.stats['cpu.limit'] = Game.cpu.limit
-Memory.stats['cpu.stats'] = Game.cpu.getUsed() - lastTick
+//Memory.stats['cpu.stats'] = Game.cpu.getUsed() - lastTick
 Memory.stats['cpu.getUsed'] = Game.cpu.getUsed()
 
