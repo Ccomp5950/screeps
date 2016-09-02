27ddafefaf9ddd19d2e19c1403b23b8e285d492f
Ccomp5950 <ccomp5950@gmail.com>
2016-09-02 01:06:40 -0400

    scouts now have setupRespawnTime

--- role.scout.js
+++ role.scout.js
@@ -2,6 +2,7 @@ module.exports = {
     // a function to run the logic for this role
     run: function(creep) {
                 if(creep.spawning) {
+                        creep.setupSpawn();
                         return;
                 }
 
@@ -14,6 +15,9 @@ module.exports = {
 			var range = creep.pos.getRangeTo(Game.flags.scout1);
 			if(range > 0) {
 				creep.moveTo(Game.flags.scout1);
+			} 
+			if (range == 1) {
+				creep.setRespawnTime();
 			}
 		}
 
