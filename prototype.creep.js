396adaa1fc3410133fdba28d40d13aa720d80a8b
Ccomp5950 <ccomp5950@gmail.com>
2016-09-06 19:23:30 -0400

    fadsfdasfa

--- prototype.creep.js
+++ prototype.creep.js
@@ -416,7 +416,7 @@ module.exports = function() {
 		}
 		for(let structureI of finder){
 			var structure = finder[structureI];
-			console.log("Structure Check: " + structure);
+			console.log("Structure Check(" + structureI + "): " + structure);
 			if(structure == undefined) {
 				continue;
 			}
