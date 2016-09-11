8dbf1ed80fad616c51300cdeb56dcc8f6a50d84a
Ccomp5950 <ccomp5950@gmail.com>
2016-09-11 02:04:18 -0400

    fdasfdfa

--- role.towerdrainer.js
+++ role.towerdrainer.js
@@ -60,6 +60,7 @@ module.exports = {
 			var range = creep.pos.getRangeTo(flag);
 			if(range > frange) {
 				creep.moveTo(flag);
+				return;
 			} else {
 				if(taunt) creep.say("Ha Missed!",true);
 				creep.setRespawnTime();
