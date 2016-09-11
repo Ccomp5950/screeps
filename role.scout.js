8fa72cc9d1a784bcc93ec57fd282b30fa0145234
Ccomp5950 <ccomp5950@gmail.com>
2016-09-11 01:52:04 -0400

    fadsfdafad

--- role.scout.js
+++ role.scout.js
@@ -6,7 +6,7 @@ module.exports = {
                         return;
                 }
 
-		if(creep.gotoWaypoint()) return;
+		//if(creep.gotoWaypoint()) return;
 
                 if(creep.getAwayFromEdge()) {
                         return;
