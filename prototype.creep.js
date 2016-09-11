834fab1c4f9518457f11cd8a0e4962745f310823
Ccomp5950 <ccomp5950@gmail.com>
2016-09-11 03:39:10 -0400

    fadsfdafda

--- prototype.creep.js
+++ prototype.creep.js
@@ -290,7 +290,7 @@ module.exports = function() {
 		switch(structure) {
 			case "FLAG":
 				if(Game.flags.priority.room != undefined && Game.flags.priority.room.name == creep.room.name) {
-					target = Game.flags.priority.pos.findClosestByRange(FIND_STRUCTURES, { filter: (s) => s.pos.getRangeTo(Game.flags.priority) < 1, maxRooms:1});
+					target = creep.pos.findClosestByRange(FIND_STRUCTURES, { filter: (s) => s.pos.getRangeTo(Game.flags.priority) < 1, maxRooms:1});
 				}
 				break;
 			case "ANYTHING":
