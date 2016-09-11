8187d6d0f639dc9a40de549574767e65931a3cf0
Ccomp5950 <ccomp5950@gmail.com>
2016-09-11 03:52:54 -0400

    fadsfdafa

--- role.defender.js
+++ role.defender.js
@@ -14,7 +14,7 @@ module.exports = {
                 if(creep.hits < creep.hitsMax) {
                         creep.heal(creep);
                 }
-		if(creep.approachAssignedFlag(999)) return;
+		if(creep.approachAssignedFlag(999) == true) return;
                 target = creep.pos.findClosestByRange(FIND_CREEPS, {
                                         filter: (c) => c.my == true && c.id != creep.id && c.hits < c.hitsMax
                         });
