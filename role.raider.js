dbeb54cccbdbc99bff2d78d555eda2b86f136313
Ccomp5950 <ccomp5950@gmail.com>
2016-09-14 14:25:34 -0400

    fadsfafa

--- role.raider.js
+++ role.raider.js
@@ -30,8 +30,9 @@ module.exports = {
 		if(creep.attackAdjacentCreep() == true) return;
 		if(creep.approachAssignedFlag(999) == false) return;
 
-                if(creep.attackHostileCreep()) return;
+                //if(creep.attackHostileCreep()) return;
 		if(creep.attackHostileStructure("FLAG")) return;
+		if(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
                 if(creep.attackHostileStructure(STRUCTURE_SPAWN)) return;
 		if(creep.attackHostileStructure(STRUCTURE_TOWER)) return;
 		if(creep.attackHostileStructure(FIND_CONSTRUCTION_SITES)) return;
