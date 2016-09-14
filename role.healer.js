fc8763e4f4ea02df246b597ba9003c262cd9496a
Ccomp5950 <ccomp5950@gmail.com>
2016-09-14 14:18:01 -0400

    fadsfdasfa

--- role.healer.js
+++ role.healer.js
@@ -11,7 +11,7 @@ module.exports = {
 
 		if(creep.memory.healing == -1) {
 			var healing  = creep.room.find(FIND_MY_CREEPS, {
-				    filter: (c) => c.memory.healer ==  -1 && c.memory.role: "raider"
+				    filter: (c) => c.memory.healer ==  -1 && c.memory.role == "raider"
 			});
 			if(healing != undefined) {
 				creep.memory.healing = healing.id;
