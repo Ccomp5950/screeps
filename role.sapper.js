7909923077eb436312eb70f2c262e3b33dbb2005
Ccomp5950 <ccomp5950@gmail.com>
2016-09-14 16:32:03 -0400

    fadsfda

--- role.sapper.js
+++ role.sapper.js
@@ -92,7 +92,7 @@ module.exports = {
 		if(creep.attackHostileStructure(STRUCTURE_SPAWN)) return;
 		if(creep.attackHostileStructure(STRUCTURE_TOWER)) return;
 		if(creep.attackHostileStructure(STRUCTURE_LINK)) return; // usually below extensions
-		(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
+		if(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
 		//if(creep.attackHostileStructure(STRUCTURE_STORAGE)) return;
 		//if(creep.attackHostileStructure(STRUCTURE_WALL)) return;
 		//if(creep.attackHostileStructure(FIND_CONSTRUCTION_SITES)) return;
