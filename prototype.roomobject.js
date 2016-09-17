4e00a16d9ace2d0d681711785f88d0894b9ba358
Ccomp5950 <ccomp5950@gmail.com>
2016-09-17 01:14:39 -0400

    adds a check for a position if a creep is on it

--- prototype.roomobject.js
+++ prototype.roomobject.js
@@ -5,4 +5,20 @@ module.exports = function() {
 		let s = this;
 		return s.pos.x.toString() + "_" + s.pos.y.toString();
 	}
+
+        RoomPosition.prototype.occupied =
+        function(creep) {
+		var ro = this;
+                var finder = ro.pos.lookFor(LOOK_CREEPS);
+                if(finder.length == 0) {
+                        return false;
+                }
+		if(finder[0].id = creep.id) {
+			return false;
+		}
+        return true;
+        };
+
+
 };
+
