d307aeae954fa478fe0b42de13e391241ebc42b4
Ccomp5950 <ccomp5950@gmail.com>
2016-09-20 22:33:45 -0400

    fdsafdfa

--- main.js
+++ main.js
@@ -118,7 +118,7 @@ module.exports.loop = function () {
 	if(Game.time % 100 == 0) {
 		console.log("[" + Game.time + "] <span style='color: yellow;'>Total Storage: " + totalstorage.toLocaleString() + "</span>");
 		let room = Game.rooms.E48S31;
-		let recepient = "E48S36";
+		let recepient = "E49S36";
 		let storage = room.storage;
 		let terminal = room.terminal;
 		let energy = storage.store.energy + terminal.store.energy;
