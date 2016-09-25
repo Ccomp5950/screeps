e333bed694aaabc3dafea0ce76a90982abc73133
Ccomp5950 <ccomp5950@gmail.com>
2016-09-25 07:25:22 -0400

    fdasfdasfa

--- role.raider.js
+++ role.raider.js
@@ -31,10 +31,10 @@ module.exports = {
 
                 //if(creep.attackHostileCreep()) return;
 		if(creep.attackHostileStructure("FLAG")) return;
-		//if(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
+		if(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
 		//if(creep.attackHostileStructure(STRUCTURE_TOWER)) return;
-                //if(creep.attackHostileStructure(STRUCTURE_SPAWN)) return;
-		//if(creep.attackHostileStructure(STRUCTURE_TOWER)) return;
+                if(creep.attackHostileStructure(STRUCTURE_SPAWN)) return;
+		if(creep.attackHostileStructure(STRUCTURE_TOWER)) return;
 		// if(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
 		//if(creep.attackHostileCreep()) return;
 		//if(creep.attackHostileStructure(FIND_CONSTRUCTION_SITES)) return;
--- role.sapper.js
+++ role.sapper.js
@@ -102,10 +102,12 @@ module.exports = {
 		// KILL SHIT
 		if(creep.attackSavedTarget()) return;
 		if(creep.attackHostileStructure("FLAG")) return;
-		if(creep.attackHostileStructure(STRUCTURE_SPAWN)) return;
-		if(creep.attackHostileStructure(STRUCTURE_TOWER)) return;
 		if(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
-		//if(creep.attackHostileStructure(STRUCTURE_STORAGE)) return;
+		if(creep.attackHostileStructure(STRUCTURE_SPAWN)) return;
+		//if(creep.attackHostileStructure(STRUCTURE_TOWER)) return;
+		//if(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
+		if(creep.attackHostileStructure(STRUCTURE_STORAGE)) return;
+		if(creep.attackHostileStructure(STRUCTURE_TOWER)) return;
 		if(creep.attackHostileStructure(STRUCTURE_LINK)) return;
 		//if(creep.attackHostileStructure(STRUCTURE_ROAD)) return;
 		//if(creep.attackHostileStructure(STRUCTURE_WALL)) return;
