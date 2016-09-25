3657bd4c8050b6498e44e001771e6b00cad0e060
Ccomp5950 <ccomp5950@gmail.com>
2016-09-25 01:08:06 -0400

    fadsfdasfas

--- functions.js
+++ functions.js
@@ -161,7 +161,7 @@
 	global.handleBuild =
 	function(roomName) {
 		let room = Game.rooms[roomName];
-		if(room.controller.my != true) {
+		if(room == undefined || room.controller.my != true || room.controller.level == undefined) {
 			return;
 		}
 		let towers = room.find(FIND_STRUCTURES, {filter: (s) => s.structureType == STRUCTURE_TOWER});
