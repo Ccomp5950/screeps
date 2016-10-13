bbf680cbd22ee0a64e05738e4ed54b78c10efcb2
Ccomp5950 <ccomp5950@gmail.com>
2016-10-13 02:10:04 -0400

    fadsfadsfa

--- role.claimer.js
+++ role.claimer.js
@@ -6,7 +6,7 @@ module.exports = {
 			creep.setupFlag();
 		}
 		if(creep.memory.MyFlag == "claimerSpot_E55S32") {
-			creep.memory.setupTime = 375;
+			creep.memory.setupTime = 400;
 		}
 		if(creep.approachAssignedFlag(0) == false) {
 			return;
