b20f7a0a8bc3a47550f90c71437ea65736fefd1f
Ccomp5950 <ccomp5950@gmail.com>
2016-12-23 23:53:51 -0500

    fdasfdafa

--- functions.js
+++ functions.js
@@ -206,7 +206,7 @@
 				}
 
 				labMem = Memory.rooms[roomM].labs[labIndex];
-				Memory.rooms.labs[labIndex].amount = labs.mineralAmount;
+				Memory.rooms[roomM].labs[labIndex].amount = labs.mineralAmount;
 				if(labMem.active != true || labMem.mineral == null || lab.cooldown > 0) {
 					continue;
 				}
