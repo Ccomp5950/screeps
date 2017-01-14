6e34d2c2b45503b883d42e1db81a6158a53e1e4b
Ccomp5950 <ccomp5950@gmail.com>
2017-01-14 04:40:13 -0500

    fdsafdfas

--- role.groundskeeper.js
+++ role.groundskeeper.js
@@ -44,7 +44,7 @@ module.exports = {
 		if(creep.repairThis(target)) return;
 
 		//Roads that are down 4000 hits.
-		target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType == STRUCTURE_ROAD && s.hits < s.hitsMax - 1000).min(s=>s.hits);
+		target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType == STRUCTURE_ROAD && s.hits < s.hitsMax).min(s=>s.hits);
 		creep.memory.lastChecked = "Roads";
 		if(creep.repairThis(target)) return;
 
