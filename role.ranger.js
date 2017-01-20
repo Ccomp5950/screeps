be9678cdc6051df3dcc56518f5c37265f54e941a
Ccomp5950 <ccomp5950@gmail.com>
2017-01-19 20:30:54 -0500

    fdsafasfas

--- role.ranger.js
+++ role.ranger.js
@@ -27,7 +27,7 @@ module.exports = {
 
 		if(!healing) {
 			target = creep.pos.findClosestByRange(FIND_CREEPS, {
-	                                        filter: (c) => c.my == true && c.id != creep.id && c.hits < c.hitsMax
+	                                        filter: (c) => c.my == true && c.id != creep.id && c.hits < c.hitsMax && c.memory.role != "skminer"
 	                        });
 	                if (target != undefined) {
 	                        if (creep.heal(target) == ERR_NOT_IN_RANGE && move) {
