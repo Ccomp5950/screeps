4c6824a60fdd660e773a3a9d26e4203bf713991a
Ccomp5950 <ccomp5950@gmail.com>
2017-01-21 22:16:04 -0500

    fdsafadsfas

--- main.js
+++ main.js
@@ -60,6 +60,9 @@ var roles =            {harvester:      {namer:"harvester",             minimum:
 
 module.exports.loop = function () {
 	profiler.wrap(function() {
+	for(let role in roles) {
+		profiler.registerObject(roles[role], role);
+	}
 	Memory.stats.tickStartCPU = Game.cpu.getUsed()
 	stats.runStats();
 	memorymgmt.master();
