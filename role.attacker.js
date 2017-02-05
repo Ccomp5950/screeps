1d1f90f99587005b0944661b08eaea746a0d1de2
Ccomp5950 <ccomp5950@gmail.com>
2017-02-05 00:01:13 -0500

    fadsfdasfa

--- role.attacker.js
+++ role.attacker.js
@@ -22,7 +22,8 @@ module.exports = {
 		//if(creep.attackAdjacentCreep() == true) return;
 		if(creep.attackHostileStructure(STRUCTURE_EXTENSION)) return;
                 if(creep.attackHostileCreep()) return;
-		//if(creep.attackHostileStructure(FIND_CONSTRUCTION_SITES)) return;
+		if(creep.attackHostileStructure(STRUCTURE_LAB)) return;
+		if(creep.attackHostileStructure(FIND_CONSTRUCTION_SITES)) return;
 		//if(creep.attackHostileStructure("FLAG")) return;
 
 
