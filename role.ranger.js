ce7259eb5ce436ca87bac6c1685d9a163abc9d77
Ccomp5950 <ccomp5950@gmail.com>
2017-02-05 02:19:27 -0500

    fdasfdasfa

--- role.ranger.js
+++ role.ranger.js
@@ -47,8 +47,10 @@ module.exports = {
 	                }
 		}
 		if(!move) return;
-		//if(creep.attackHostileStructure(STRUCTURE_ROAD)) return;
-		//if(creep.attackHostileStructure(FIND_CONSTRUCTION_SITES)) return;
+		if(creep.pos.getRangeTo(flag) < 999) {
+			if(creep.attackHostileStructure(STRUCTURE_ROAD)) return;
+			if(creep.attackHostileStructure(FIND_CONSTRUCTION_SITES)) return;
+		}
 		creep.approachAssignedFlag(0);
 
         }
