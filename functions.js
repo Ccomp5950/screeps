59a9aa8ac78aa65cfc00bcc5a2eb51a35460e87f
Ccomp5950 <ccomp5950@gmail.com>
2017-02-11 23:33:05 -0500

    adding a new function

--- functions.js
+++ functions.js
@@ -285,6 +285,24 @@
 
 		return nuke;
 	}
+	global.toUTF16 =
+	function(codePoint) {
+		var TEN_BITS = parseInt('1111111111', 2);
+		function u(codeUnit) {
+			return '\\u'+codeUnit.toString(16).toUpperCase();
+		}
+
+		if (codePoint <= 0xFFFF) {
+			return u(codePoint);
+		}
+		codePoint -= 0x10000;
+	        // Shift right to get to most significant 10 bits
+		var leadSurrogate = 0xD800 + (codePoint >> 10);
+		// Mask to get least significant 10 bits
+		var tailSurrogate = 0xDC00 + (codePoint & TEN_BITS);
+		return u(leadSurrogate) + u(tailSurrogate);
+	}
+
 
 	global.handleBuild =
 	function(roomName) {
