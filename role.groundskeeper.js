36b6e5c9dc6f187de1bfac6376398ae159fa8341
Ccomp5950 <ccomp5950@gmail.com>
2017-03-10 04:28:26 -0500

    Grounds Keepers now handle walls and ramparts like repairers

--- role.groundskeeper.js
+++ role.groundskeeper.js
@@ -4,6 +4,7 @@ module.exports = {
                 if(creep.spawning) {
                         return;
                 }
+	 creep.memory.currentRole = "repairer";
 	creep.setupFlag();
         // if creep is trying to repair something but has no energy left
         if (creep.memory.working == true && creep.carry.energy == 0) {
@@ -39,12 +40,12 @@ module.exports = {
 		if(creep.repairThis(target)) return;
 
 		//Containers
-                target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType == STRUCTURE_CONTAINER && s.hits < s.hitsMax - 50000).min(s=>s.hits);
+                target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType == STRUCTURE_CONTAINER && s.hits < s.hitsMax - 50000 && s.isBeingHandled(creep) == false).min(s=>s.hits);
 		creep.memory.lastChecked = "Containers";
 		if(creep.repairThis(target)) return;
 
 		//Roads that are down 4000 hits.
-		target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType == STRUCTURE_ROAD && s.hits < s.hitsMax).min(s=>s.hits);
+		target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType == STRUCTURE_ROAD && s.hits < s.hitsMax && s.isBeingHandled(creep) == false).min(s=>s.hits);
 		creep.memory.lastChecked = "Roads";
 		if(creep.repairThis(target)) return;
 
@@ -52,7 +53,7 @@ module.exports = {
 		creep.memory.lastChecked = "Everything but walls and ramparts";
 		target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType != STRUCTURE_WALL 
 									&& s.structureType != STRUCTURE_RAMPART 
-									&& s.hits < s.hitsMax).min(s=> s.hits/s.hitsMax);
+									&& s.hits < s.hitsMax && s.isBeingHandled(creep) == false).min(s=> s.hits/s.hitsMax);
 		if(creep.repairThis(target)) return;
 
 		// CONSTRUCTION TIME.
@@ -63,22 +64,28 @@ module.exports = {
 
                         if(creep.buildThis(target)) return;
                 }
-		// Ramparts
-		creep.memory.lastChecked = "Ramparts";
-		target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType == STRUCTURE_RAMPART
-									&& s.hits < rampartMinHealth).min(s=>s.hits);
-		if(creep.repairThis(target)) return;
 
-		// Walls.
-		creep.memory.lastChecked = "Walls";
-		target = _(creep.room.find(FIND_STRUCTURES)).filter((s) => s.structureType == STRUCTURE_WALL
-									&& s.hits < wallMinHealth).min(s=>s.hits);
-                if(creep.repairThis(target)) return;
-		creep.memory.lastChecked = "Nothing to repair/build";
 
+		storage = creep.room.storage;
+		creep.memory.lastChecked = "Ramparts and Walls";
+		if(storage == undefined || storage.store.energy > 50000) {
+			var hitsMax = RAMPART_HITS_MAX[creep.room.controller.level];
+			var config_hitsMax = getMaxDefenseHits();
+			if(hitsMax > config_hitsMax) hitsMax = config_hitsMax;
+			target = _(creep.room.find(FIND_STRUCTURES))
+			.filter((s) => (s.structureType == STRUCTURE_RAMPART || s.structureType == STRUCTURE_WALL) && s.hits < hitsMax && s.hits < s.hitsMax && s.isBeingHandled(creep) == false)
+			.min(s=>s.hits / hitsMax);
+			if(structure != Infinity) {
+				f(creep.repairThis(target)) return;
+			}
+
+																										                            }
+
+		creep.memory.lastChecked = "Heading to flag";
 		creep.approachAssignedFlag(0);
         }
         else {
+		creep.memory.lastChecked = "Grabbing energy";
 		creep.customharvest();
         }
 
